18272a68917cc24c6395eb4d1dfd28951cebfed6
Cauã <cauayves5@gmail.com>
2023-01-10 23:49:55 -0300

    distribuicao de cartas 2 de 3

--- library.js
+++ library.js
@@ -1,5 +1,10 @@
 function start(){
     let initCards = Number(prompt('Com quantas cartas quer jogar?'))
 
-    
+    if(initCards > 3 && initCards <15 && initCards % 2 == 0){
+        alert(`inciando com ${initCards}`)
+    }else{
+        alert('Quantidade inválida!')
+        start()
+    }
 }
